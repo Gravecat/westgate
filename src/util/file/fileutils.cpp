32d6b55da36339266309642dabedcbe2436959e0
Raine 'Gravecat' Simmons <gc@gravecat.com>
2025-11-19 21:47:14 +0000

    Added a file_to_vec() function in fileutils.

--- src/util/file/fileutils.cpp
+++ src/util/file/fileutils.cpp
@@ -25,4 +25,22 @@ std::string file_to_string(const std::string &filename)
     return buffer.str();
 }
 
+// Loads a text file into a vector, one string for each line of the file.
+std::vector<std::string> file_to_vec(const std::string &filename)
+{
+    if (!std::filesystem::exists(filename)) throw std::runtime_error("Invalid file: " + filename);
+    std::vector<std::string> lines;
+    std::ifstream file(filename);
+    if (!file.is_open()) throw std::runtime_error("Cannot open file: " + filename);
+
+    std::string line;
+    while (std::getline(file, line))
+    {
+        if (!line.empty() && (line.back() == '\r' || line.back() == '\n')) line.pop_back();
+        lines.push_back(line);
+    }
+    file.close();
+    return lines;
+}
+
 } } // fileutils, lom namespaces
