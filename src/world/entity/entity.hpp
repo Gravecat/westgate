358a3492bf86cbb9cb3a1e55098cb1876174ac7b
Raine 'Gravecat' Simmons <gc@gravecat.com>
2025-11-20 20:01:02 +0000

    Added a Room class, to define locations in the game world.

--- src/world/entity/entity.hpp
+++ src/world/entity/entity.hpp
@@ -11,6 +11,8 @@
 
 namespace lom {
 
+class Room; // defined in world/room.hpp
+
 // The genders of things in the game.
 enum class Gender : uint8_t { NONE, HE, SHE, THEY, IT };
 
@@ -20,12 +22,19 @@ public:
     virtual             ~Entity() = default;    // Virtual destructor. Nothing here yet.
     Gender              gender() const; // Retrieves the gender (if any) of this Entity.
     const std::string&  name() const;   // Retrieves the name of this Entity.
+    const Entity*       parent_entity() const;  // Retrieves the Entity (if any) containing this Entity.
+    const Room*         parent_room() const;    // Retrieves the Room (if any) containing this Entity.
     void                set_gender(Gender new_gender);  // Sets the gender of this Entity.
     void                set_name(const std::string& new_name);  // Sets the name of this Entity.
+    void                set_parent_entity(Entity* new_entity_parent = nullptr); // Sets a new Entity as the parent of this Entity, or nullptr for none.
+    void                set_parent_room(Room* new_room_parent = nullptr);       // Sets a new Room as the parent of this Entity, or nullptr for none.
+    const Vector2       world_pos() const;  // Retrieves the world coordinates of the parent of this Entity, or {0,0} if no parent is defined.
 
 protected:
-    Gender      gender_;    // The gender of this Entity, if any.
-    std::string name_;      // Every Entity must be called something.
+    Gender      gender_;        // The gender of this Entity, if any.
+    std::string name_;          // Every Entity must be called something.
+    Entity*     parent_entity_; // The Entity (if any) containing this Entity.
+    Room*       parent_room_;   // The Room (if any) where this Entity is located.
 };
 
 }   // namespace lom
---
+++ src/world/room.hpp
@@ -0,0 +1,35 @@
+// world/room.hpp -- The Room class defines a singular place in the game world, connected via links to other Rooms. Entities all exist within Rooms.
+
+// SPDX-FileType: SOURCE
+// SPDX-FileCopyrightText: Copyright 2025 Raine Simmons <gc@gravecat.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+#pragma once
+
+#include "core/global.hpp"
+
+namespace lom {
+
+class Mobile;   // defined in entity/mobile.hpp
+
+class Room {
+public:
+                        Room(); // Creates a blank Room with default values.
+    const std::string&  desc() const;   // Retrieves the description of this Room.
+    const std::string&  name() const;   // Retrieves the name of this Room.
+    void                set_desc(const std::string& new_desc);  // Sets the description of this Room.
+    void                set_name(const std::string& new_name);  // Sets the name of this Room.
+    void                set_world_pos(const Vector2 new_pos);   // Sets the new world coordinates for this Room.
+    void                transfer(Entity* entity_ptr, Room* room_ptr);   // Transfers a specified Entity from this Room to a target Room.
+    const Vector2       world_pos() const;  // Retrieves the world coordinates of this Room.
+
+protected:
+    std::vector<std::unique_ptr<Entity>>    entities_;  // Entities that exist within this Room.
+
+private:
+    std::string desc_;      // The text description of this Room, as shown to the player.
+    std::string name_;      // The name of this Room.
+    Vector2     world_pos_; // The X/Y coordinates of this Room in the game's world-space. Negative values are permitted, but {0,0} are forbidden coordinates.
+};
+
+}   // namespace lom
