5004cbe8e9d122ea8120ea3911f09bc4ede83627
Raine 'Gravecat' Simmons <gc@gravecat.com>
2025-11-20 20:01:02 +0000

    Added a Room class, to define locations in the game world.

--- src/world/entity/entity.cpp
+++ src/world/entity/entity.cpp
@@ -7,17 +7,24 @@
 
 #include "core/core.hpp"
 #include "world/entity/entity.hpp"
+#include "world/room.hpp"
 
 namespace lom {
 
 // Creates a blank Entity with default values.
-Entity::Entity() : gender_(Gender::NONE), name_("undefined entity") { }
+Entity::Entity() : gender_(Gender::NONE), name_("undefined entity"), parent_entity_(nullptr), parent_room_(nullptr) { }
 
 // Retrieves the gender (if any) of this Entity.
 Gender Entity::gender() const { return gender_; }
 
 // Retrieves the name of this Entity.
 const std::string& Entity::name() const { return name_; }
+
+// Retrieves the Entity (if any) containing this Entity.
+const Entity* Entity::parent_entity() const { return parent_entity_; }
+
+// Retrieves the Room (if any) containing this Entity.
+const Room* Entity::parent_room() const { return parent_room_; }
 
 // Sets the gender of this Entity.
 void Entity::set_gender(Gender new_gender)
@@ -33,5 +40,42 @@ void Entity::set_gender(Gender new_gender)
 
 // Sets the name of this Entity.
 void Entity::set_name(const std::string& new_name) { name_ = new_name; }
+
+// Sets a new Entity as the parent of this Entity, or nullptr for none.
+void Entity::set_parent_entity(Entity* new_entity_parent)
+{
+    if (new_entity_parent == this)
+    {
+        core().nonfatal("Attempt to set entity parent to itself (" + name_ + ")", Core::CORE_ERROR);
+        new_entity_parent = nullptr;
+    }
+    parent_entity_ = new_entity_parent;
+    if (new_entity_parent) set_parent_room(nullptr);    // An Entity can only have one parent.
+}
+
+// Sets a new Room as the parent of this Entity, or nullptr for none.
+void Entity::set_parent_room(Room* new_room_parent)
+{
+    parent_room_ = new_room_parent;
+    if (new_room_parent) set_parent_entity(nullptr);    // An Entity can only have one parent.
+}
+
+// Retrieves the world coordinates of the parent of this Entity, or {0,0} if no parent is defined.
+const Vector2 Entity::world_pos() const
+{
+    if (parent_room_) return parent_room_->world_pos();
+    else if (parent_entity_)
+    {
+        // Sanity check to avoid an infinite loop. This will only really be possible if something was badly broken in the code, but it'll help with debugging.
+        // It should be impossible for this to happen, but hey, it's such a simple quick check that'll avoid some nasty infinite recursion.
+        if (parent_entity_ == this)
+        {
+            core().nonfatal("Attempt to check world position of Entity parented to itself (" + name_ + ")", Core::CORE_ERROR);
+            return {0,0};
+        }
+        return parent_entity_->world_pos();
+    }
+    else return {0,0};
+}
 
 }   // namespace lom
---
+++ src/world/room.cpp
@@ -0,0 +1,118 @@
+// world/room.cpp -- The Room class defines a singular place in the game world, connected via links to other Rooms. Entities all exist within Rooms.
+
+// SPDX-FileType: SOURCE
+// SPDX-FileCopyrightText: Copyright 2025 Raine Simmons <gc@gravecat.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+#include "core/core.hpp"
+#include "world/entity/mobile.hpp"
+#include "world/room.hpp"
+
+namespace lom {
+
+// Creates a blank Room with default values.
+Room::Room() : desc_("Missing room description."), name_("Undefined Room"), world_pos_({0,0}) { }
+
+// Retrieves the description of this Room.
+const std::string& Room::desc() const { return desc_; }
+
+// Retrieves the name of this Room.
+const std::string& Room::name() const { return name_; }
+
+// Sets the description of this Room.
+void Room::set_desc(const std::string& new_desc)
+{
+    if (!new_desc.size())
+    {
+        core().nonfatal("Attempt to set blank description on room (" + name_ + ")", Core::CORE_ERROR);
+        desc_ = "Missing room description.";
+    }
+    else desc_ = new_desc;
+}
+
+// Sets the name of this Room.
+void Room::set_name(const std::string& new_name)
+{
+    if (!new_name.size())
+    {
+        core().nonfatal("Attempt to set blank name on room.", Core::CORE_ERROR);
+        name_ = "Undefined Room";
+    }
+    else name_ = new_name;
+}
+
+// Sets the new world coordinates for this Room.
+void Room::set_world_pos(const Vector2 new_pos)
+{
+    if (!new_pos)
+    {
+        core().nonfatal("Attempt to set {0,0} coordinates on room (" + name_ + ")", Core::CORE_ERROR);
+        return;
+    }
+    world_pos_ = new_pos;
+}
+
+// Transfers a specified Mobile from this Room to a target Room.
+void Room::transfer(Entity* entity_ptr, Room* room_ptr)
+{
+    // First, sanity checks. These should never happen, but could possibly occur as the result of mistakes in the code.
+    if (room_ptr == this)
+    {
+        if (!entity_ptr) core().nonfatal("Attempt to transfer null entity from " + name_ + " to itself.", Core::CORE_ERROR);
+        else core().nonfatal("Attempt to transfer enity (" + entity_ptr->name() + ") from " + name_ + " to itself.", Core::CORE_ERROR);
+        return;
+    }
+    if (!entity_ptr)
+    {
+        if (!room_ptr) core().nonfatal("Attempt to transfer null entity from " + name_ + " to null room.", Core::CORE_ERROR);
+        else core().nonfatal("Attempt to transfer null entity from " + name_ + " to " + room_ptr->name() + ".", Core::CORE_ERROR);
+        return;
+    }
+    if (!room_ptr)
+    {
+        if (!entity_ptr) core().nonfatal("Attempt to transfer null entity from " + name_ + " to null room.", Core::CORE_ERROR);
+        else core().nonfatal("Attempt to transfer entity (" + entity_ptr->name() + ") from " + name_ + " to null room.", Core::CORE_ERROR);
+        return;
+    }
+    if (entity_ptr->parent_room() != this)
+    {
+        core().nonfatal("Attempt to transfer entity (" + entity_ptr->name() + ") from " + name_ + " to " + room_ptr->name() +
+            " while entity is not correctly parented to this room.", Core::CORE_ERROR);
+        return;
+    }
+
+    // Try to determine which vector position houses the Entity being moved.
+    uint32_t source_id = 0;
+    bool source_found = false;
+    for (unsigned int i = 0; i < entities_.size(); i++)
+    {
+        if (entities_.at(i).get() == entity_ptr)
+        {
+            source_id = i;
+            source_found = true;
+            break;
+        }
+    }
+    if (!source_found)
+    {
+        core().nonfatal("Attempt to transfer entity (" + entity_ptr->name() + ") from " + name_ + " to " + room_ptr->name() +
+            ", while entity is not contained within the parent room.", Core::CORE_ERROR);
+        return;
+    }
+
+    // We now know the position in the entities_ vector for the requested entity, and all the pointers appear to be in order. Let's transfer ownership...
+    room_ptr->entities_.push_back(std::move(entities_[source_id]));
+    // ...and immediately delete the now-invalid pointer from our own entities_ vector...
+    entities_.erase(entities_.begin() + source_id);
+    // ...and finally, update the Entity's parent Room pointer.
+    entity_ptr->set_parent_room(room_ptr);
+}
+
+// Retrieves the world coordinates of this Room.
+const Vector2 Room::world_pos() const
+{
+    if (!world_pos_) core().nonfatal("Coordinate lookup for room at {0,0} (" + name_ + ")", Core::CORE_WARN);
+    return world_pos_;
+}
+
+}   // namespace lom
