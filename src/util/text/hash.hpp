5bb88c67f728030bca05b8784b85507f244e1345
Raine 'Gravecat' Simmons <gc@gravecat.com>
2025-11-22 21:44:59 +0000

    Added real-time hash collision checks that only run on debug builds.

--- src/util/text/hash.hpp
+++ src/util/text/hash.hpp
@@ -12,4 +12,10 @@ namespace hash {
 
 uint32_t    murmur3(const std::string& str);   // Hashes a string with MurmurHash3.
 
+// Only in debug builds, we're gonna add some extra code to detect hash collisions in real-time. Yes, it'll slow performance by a tiny amount, but it's a
+// debug build, we're not expecting maximum optimization and speed here.
+#ifdef WESTGATE_BUILD_DEBUG
+void        check_hash_collision(const std::string& str, uint32_t hash);
+#endif
+
 } }   // hash, westgate namespaces
