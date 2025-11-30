dbaedb8af3e2b2029c3a25169b7db546a8cfbb0a
Raine 'Gravecat' Simmons <gc@gravecat.com>
2025-11-30 19:57:57 +0000

    Added a #weather debug command to preview weather/time room descriptions.

--- src/actions/cheats.cpp
+++ src/actions/cheats.cpp
@@ -6,8 +6,12 @@
 
 #include "core/terminal.hpp"
 #include "actions/cheats.hpp"
+#include "trailmix/text/manipulation.hpp"
+#include "world/time/time-weather.hpp"
+#include "world/world.hpp"
 
 using std::to_string;
+using trailmix::text::manipulation::str_toupper;
 using westgate::terminal::print;
 
 namespace westgate::actions::cheats {
@@ -21,6 +25,17 @@ void hash(PARSER_FUNCTION)
         return;
     }
     print("The hashed version of {C}" + words.at(1) + " {w}is {C}" + to_string(words_hashed.at(1)));
+}
+
+// Retrieves a specified weather-map description.
+void weather_desc(PARSER_FUNCTION)
+{ PARSER_NO_HASHED
+    if (words.size() < 2)
+    {
+        print("{Y}Please specify a weather string to print.");
+        return;
+    }
+    print("{K}" + world().time_weather().string_map(str_toupper(words.at(1))));
 }
 
 }   // namespace westgate::actions::cheats
